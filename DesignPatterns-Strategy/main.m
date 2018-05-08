3888e0d62945aad654c05645dc7782f6c3a7d5cc
renjinwei <renjwjx@gmail.com>
2018-05-08 21:14:22 +0800

    Initial Commit

---
+++ DesignPatterns-Strategy/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DesignPatterns-Strategy
+//
+//  Created by jinren on 08/05/2018.
+//  Copyright © 2018 jinren. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
